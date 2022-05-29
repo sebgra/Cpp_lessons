16d720e48c4e431cca0fb3a8d710469ca98ca1ae
sebgra <sebastiengradit@hotmail.com>
2022-05-29 19:42:31 +0200

    Added blan Chapter_5

--- create.sh
+++ create.sh
@@ -1,53 +1,74 @@
-mkdir Chapter_4
-cd Chapter_4
+mkdir Chapter_5
+cd Chapter_5
 
-mkdir Chapter_4_1
-cd Chapter_4_1
+mkdir Chapter_5_1
+cd Chapter_5_1
 touch CMakeLists.txt
-touch Chapter_4_1.cpp
-touch Chapter_4_1.h
+touch Chapter_5_1.cpp
+touch Chapter_5_1.h
 cd ../
 
-mkdir Chapter_4_2
-cd Chapter_4_2
+mkdir Chapter_5_2
+cd Chapter_5_2
 touch CMakeLists.txt
-touch Chapter_4_2.cpp
-touch Chapter_4_2.h
+touch Chapter_5_2.cpp
+touch Chapter_5_2.h
 cd ../
 
-mkdir Chapter_4_3
-cd Chapter_4_3
+mkdir Chapter_5_3
+cd Chapter_5_3
 touch CMakeLists.txt
-touch Chapter_4_3.cpp
-touch Chapter_4_3.h
+touch Chapter_5_3.cpp
+touch Chapter_5_3.h
 cd ../
 
-mkdir Chapter_4_4
-cd Chapter_4_4
+mkdir Chapter_5_4
+cd Chapter_5_4
 touch CMakeLists.txt
-touch Chapter_4_4.cpp
-touch Chapter_4_4.h
+touch Chapter_5_4.cpp
+touch Chapter_5_4.h
 cd ../
 
-mkdir Chapter_4_5
-cd Chapter_4_5
+mkdir Chapter_5_5
+cd Chapter_5_5
 touch CMakeLists.txt
-touch Chapter_4_5.cpp
-touch Chapter_4_5.h
+touch Chapter_5_5.cpp
+touch Chapter_5_5.h
 cd ../
 
-mkdir Chapter_4_6
-cd Chapter_4_6
+mkdir Chapter_5_6
+cd Chapter_5_6
 touch CMakeLists.txt
-touch Chapter_4_6.cpp
-touch Chapter_4_6.h
+touch Chapter_5_6.cpp
+touch Chapter_5_6.h
 cd ../
 
-mkdir Chapter_4_7
-cd Chapter_4_7
+mkdir Chapter_5_7
+cd Chapter_5_7
 touch CMakeLists.txt
-touch Chapter_4_7.cpp
-touch Chapter_4_7.h
+touch Chapter_5_7.cpp
+touch Chapter_5_7.h
+cd ../
+
+mkdir Chapter_5_8
+cd Chapter_5_8
+touch CMakeLists.txt
+touch Chapter_5_8.cpp
+touch Chapter_5_8.h
+cd ../
+
+mkdir Chapter_5_9
+cd Chapter_5_9
+touch CMakeLists.txt
+touch Chapter_5_9.cpp
+touch Chapter_5_9.h
+cd ../
+
+mkdir Chapter_5_10
+cd Chapter_5_10
+touch CMakeLists.txt
+touch Chapter_5_10.cpp
+touch Chapter_5_10.h
 cd ../
 
 
