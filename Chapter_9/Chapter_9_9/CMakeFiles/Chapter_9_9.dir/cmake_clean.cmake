02944a1d97291429bd89ddfdb14ccf6562b744d9
sebgra <sebastiengradit@hotmail.com>
2022-09-17 13:58:53 +0200

    Added Chapter 9.9

---
+++ Chapter_9/Chapter_9_9/CMakeFiles/Chapter_9_9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_9_9.dir/Chapter_9_9.cpp.o"
+  "CMakeFiles/Chapter_9_9.dir/Chapter_9_9.cpp.o.d"
+  "Chapter_9_9"
+  "Chapter_9_9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter_9_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter_9/Chapter_9_9/Chapter_9_9.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class MyString{
+
+    private :
+
+        char* buffer;
+
+    public :
+
+        MyString(const char* initString){ // constructor
+
+            buffer = NULL;
+
+            cout << "Default constructor : creating new MyString" << endl;
+
+            if (initString != NULL){
+
+                buffer = new char[strlen(initString) + 1];
+                strcpy(buffer, initString);
+                
+                cout << "buffer points to: 0x" << hex;
+                cout << (unsigned int*)buffer << endl;
+                }
+
+}
+
+
+
+MyString(const MyString& copySource){ // Copy constructor
+
+    buffer = NULL;
+    cout << "Copy constructor : copying from MyString" << endl;
+    if (copySource.buffer != NULL){
+
+        // allocate own buffer
+
+        buffer = new char[strlen(copySource.buffer) + 1];
+        
+        // deep copy from the source into local buffer
+        
+        strcpy(buffer, copySource.buffer);
+
+        cout << "buffer points to: 0x" << hex;
+        cout << (unsigned int*)buffer << endl;
+
+        }
+
+}
+
+// Destructor
+
+~MyString(){
+
+    cout << "Invoking destructor, clearing up"  << endl;
+    delete [] buffer;
+}
+
+int GetLength(){
+
+    return strlen(buffer);
+}
+
+const char* GetString(){
+    return buffer;
+}
+
+};
+
+void UseMyString(MyString str){
+
+    cout << "String buffer in MyString is " << str.GetLength();
+    cout << "characters long" << endl;
+
+    cout << "buffer contains: " << str.GetString() << endl;
+    return;
+}
+
+int main(){
+
+    MyString sayHello("Hello from String class");
+    UseMyString(sayHello);
+
+    return 0;
+
+}
